685a4db251b3e798c5af1ab6e91c843ff3e201c7
rgbk21 <rajgaurav.bk@gmail.com>
2020-08-19 20:56:39 -0400

    Service is wired up and ready to consume

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// CLI doesn’t automatically register the Stocks service with the App module,
+// and we need to register HttpClient with the application as well.
+import { HttpClientModule } from '@angular/common/http';
+import { StocksService } from './services/stocks.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component'; // Imports the App Component
 
@@ -9,11 +14,16 @@ import { AppComponent } from './app.component'; // Imports the App Component
   declarations: [ // Declarations are to list any components and directives used in the app
     AppComponent
   ],
+  // The import statements on line 7,8 will import the Stocks service and HttpClientModule into the file,
+  // but we need to register the HttpClientModule with the application.
   imports: [ // Imports are other modules that are used in the app
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [], // Providers are any services that are used in the app
+  // we need to register the new StocksService with the providers property to
+  // inform Angular that it should be made available for the module to use
+  providers: [StocksService], // Providers are any services that are used in the app
   bootstrap: [AppComponent] // Bootstrap declares which component to use as the first to bootstrap the application.
 })
 export class AppModule { } // Exports an empty class that gets annotated with configuration from NgModule
---
+++ src/app/services/stocks.service.ts
@@ -0,0 +1,48 @@
+// Import Dependencies
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+// Declare a stock array and API variables
+let stocks : Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR'];
+let service: string = 'https://angular2-in-action-api.herokuapp.com';
+
+// Define and export the TypeScript interface for the stock object
+export interface StockInterface {
+  symbol: string;
+  lastTradePriceOnly: number;
+  change: number;
+  changeInPercent: number;
+}
+
+// Annotated with @Injectable to wire up dependency injection
+@Injectable()
+export class StocksService { // Defines class and exports it
+
+  // Constructor method to inject HttpClient service into class http property
+  constructor(private http: HttpClient) { }
+
+  // Method to get the Stocks
+  // Note that the method returns a copy of the stocks
+  get() {
+    return stocks.slice();
+  }
+
+  // Method to add a new Stock to the List
+  add(stock: string) {
+    stocks.push(stock);
+    return this.get();
+  }
+
+  // Method to remove a Stock from the List
+  remove(stock: string) {
+    stocks.splice(stocks.indexOf(stock), 1);
+    return this.get();
+  }
+
+  // Method to load HttpClient service to load sock values from the API
+  load(symbols) {
+    if (symbols) {
+      return this.http.get<Array<StockInterface>>(service + '/stocks/snapshot?symbols=' + symbols.join());
+    }
+  }
+}
